27e2b9ad9bff113e178bd0947fa0c55b52e4a329
ryankawahara <64173139+ryankawahara@users.noreply.github.com>
2023-10-01 23:41:48 -0400

    First updates
    Getting it working, added file for neutral

--- demos/demo_reconstruct_neutral.py
+++ demos/demo_reconstruct_neutral.py
@@ -28,9 +28,11 @@ from decalib.datasets import datasets
 from decalib.utils import util
 from decalib.utils.config import cfg as deca_cfg
 from decalib.utils.tensor_cropper import transform_points
+from decalib.utils.rotation_converter import batch_euler2axis, deg2rad
 
 
 def main(args):
+    expdata = datasets.TestData(args.exp_path, iscrop=args.iscrop, face_detector=args.detector)
     # if args.rasterizer_type != 'standard':
     #     args.render_orig = False
     savefolder = args.savefolder
@@ -38,33 +40,50 @@ def main(args):
     os.makedirs(savefolder, exist_ok=True)
 
     # load test images
-    testdata = datasets.TestData(args.inputpath, iscrop=args.iscrop, face_detector=args.detector,
-                                 sample_step=args.sample_step)
+    testdata = datasets.TestData(args.inputpath, iscrop=args.iscrop, face_detector=args.detector, sample_step=args.sample_step)
 
     # run DECA
     deca_cfg.model.use_tex = args.useTex
     deca_cfg.rasterizer_type = args.rasterizer_type
     deca_cfg.model.extract_tex = args.extractTex
-    deca = DECA(config=deca_cfg, device=device)
+    deca = DECA(config = deca_cfg, device=device)
     # for i in range(len(testdata)):
     for i in tqdm(range(len(testdata))):
         name = testdata[i]['imagename']
-        images = testdata[i]['image'].to(device)[None, ...]
+        images = testdata[i]['image'].to(device)[None,...]
         with torch.no_grad():
             codedict = deca.encode(images)
-            opdict, visdict = deca.decode(codedict)  # tensor
+
+            exp_images = expdata[1]['image'].to(device)[None,...]
+            exp_codedict = deca.encode(exp_images)
+            # transfer exp code
+            codedict['pose'][:,3:] = exp_codedict['pose'][:,3:]
+            codedict['exp'] = exp_codedict['exp']
+
+            euler_pose = torch.zeros((1, 3))
+            global_pose = batch_euler2axis(deg2rad(euler_pose[:, :3].cuda()))
+            codedict['pose'][:,:3] = global_pose
+
+
+            print("the current neck rotation is", codedict['pose'][:,:3])
+
+            print("POSE", codedict['pose'])
+            opdict, visdict = deca.decode(codedict) #tensor
             if args.render_orig:
                 tform = testdata[i]['tform'][None, ...]
-                tform = torch.inverse(tform).transpose(1, 2).to(device)
+                tform = torch.inverse(tform).transpose(1,2).to(device)
                 original_image = testdata[i]['original_image'][None, ...].to(device)
                 _, orig_visdict = deca.decode(codedict, render_orig=True, original_image=original_image, tform=tform)
                 orig_visdict['inputs'] = original_image
+                print(orig_visdict.keys())
+                print("opdict", opdict.keys())
+                print("visdict", visdict.keys())
 
         if args.saveDepth or args.saveKpt or args.saveObj or args.saveMat or args.saveImages:
             os.makedirs(os.path.join(savefolder, name), exist_ok=True)
         # -- save results
         if args.saveDepth:
-            depth_image = deca.render.render_depth(opdict['trans_verts']).repeat(1, 3, 1, 1)
+            depth_image = deca.render.render_depth(opdict['trans_verts']).repeat(1,3,1,1)
             visdict['depth_images'] = depth_image
             cv2.imwrite(os.path.join(savefolder, name, name + '_depth.jpg'), util.tensor2image(depth_image[0]))
         if args.saveKpt:
@@ -80,19 +99,15 @@ def main(args):
             if args.render_orig:
                 cv2.imwrite(os.path.join(savefolder, name + '_vis_original_size.jpg'), deca.visualize(orig_visdict))
         if args.saveImages:
-            for vis_name in ['inputs', 'rendered_images', 'albedo_images', 'shape_images', 'shape_detail_images',
-                             'landmarks2d']:
+            for vis_name in ['inputs', 'rendered_images', 'albedo_images', 'shape_images', 'shape_detail_images', 'landmarks2d']:
                 if vis_name not in visdict.keys():
                     continue
                 image = util.tensor2image(visdict[vis_name][0])
-                cv2.imwrite(os.path.join(savefolder, name, name + '_' + vis_name + '.jpg'),
-                            util.tensor2image(visdict[vis_name][0]))
+                cv2.imwrite(os.path.join(savefolder, name, name + '_' + vis_name +'.jpg'), util.tensor2image(visdict[vis_name][0]))
                 if args.render_orig:
                     image = util.tensor2image(orig_visdict[vis_name][0])
-                    cv2.imwrite(os.path.join(savefolder, name, 'orig_' + name + '_' + vis_name + '.jpg'),
-                                util.tensor2image(orig_visdict[vis_name][0]))
+                    cv2.imwrite(os.path.join(savefolder, name, 'orig_' + name + '_' + vis_name +'.jpg'), util.tensor2image(orig_visdict[vis_name][0]))
     print(f'-- please check the results in {savefolder}')
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DECA: Detailed Expression Capture and Animation')
@@ -102,36 +117,40 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--savefolder', default='TestSamples/examples/results', type=str,
                         help='path to the output directory, where results(obj, txt files) will be stored.')
     parser.add_argument('--device', default='cuda', type=str,
-                        help='set device, cpu for using cpu')
+                        help='set device, cpu for using cpu' )
     # process test images
     parser.add_argument('--iscrop', default=True, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to crop input image, set false only when the test image are well cropped')
+                        help='whether to crop input image, set false only when the test image are well cropped' )
     parser.add_argument('--sample_step', default=10, type=int,
-                        help='sample images from video data for every step')
+                        help='sample images from video data for every step' )
     parser.add_argument('--detector', default='fan', type=str,
-                        help='detector for cropping face, check decalib/detectors.py for details')
+                        help='detector for cropping face, check decalib/detectors.py for details' )
     # rendering option
     parser.add_argument('--rasterizer_type', default='standard', type=str,
-                        help='rasterizer type: pytorch3d or standard')
+                        help='rasterizer type: pytorch3d or standard' )
     parser.add_argument('--render_orig', default=True, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to render results in original image size, currently only works when rasterizer_type=standard')
     # save
     parser.add_argument('--useTex', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to use FLAME texture model to generate uv texture map, \
-                            set it to True only if you downloaded texture model')
+                            set it to True only if you downloaded texture model' )
     parser.add_argument('--extractTex', default=True, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to extract texture from input image as the uv texture map, set false if you want albeo map from FLAME mode')
+                        help='whether to extract texture from input image as the uv texture map, set false if you want albeo map from FLAME mode' )
     parser.add_argument('--saveVis', default=True, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save visualization of output')
+                        help='whether to save visualization of output' )
     parser.add_argument('--saveKpt', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save 2D and 3D keypoints')
+                        help='whether to save 2D and 3D keypoints' )
     parser.add_argument('--saveDepth', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save depth image')
+                        help='whether to save depth image' )
     parser.add_argument('--saveObj', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to save outputs as .obj, detail mesh will end with _detail.obj. \
-                            Note that saving objs could be slow')
+                            Note that saving objs could be slow' )
     parser.add_argument('--saveMat', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save outputs as .mat')
+                        help='whether to save outputs as .mat' )
     parser.add_argument('--saveImages', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save visualization output as seperate images')
+                        help='whether to save visualization output as seperate images' )
+
+    parser.add_argument('-e', '--exp_path', default='TestSamples/exp', type=str,
+                        help='path to expression')
+
     main(parser.parse_args())
